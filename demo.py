dade08056cf39027cf078dd069175e2870a0fa2e
bobo_test <1716470113@qq.com>
2021-11-08 01:59:08 +0800

    添加操作步骤详情

--- demo.py
+++ demo.py
@@ -2,7 +2,14 @@
 git remote add origin https://github.com/Mr-RollBoby/git_test.git
 git branch -M main
 git push -u origin main
+set:
+1.idea path like this -> vcs to import into version cantral,then to Create Git Repositary
+select your project click open ,then your file is red.
+2.right click your project name,find git to Repositary select Remotes
+3.add your gitURL,this URL is top line! click OK 现在已经将git和idea关联
+
 """
+
 a=1
 b=2
 a,b = b,a
